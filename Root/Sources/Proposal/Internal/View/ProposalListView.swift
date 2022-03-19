2e13b1d95cccb8b0a2b1370e3cfa1ae16c6f242b
Yusuke Hosonuma <tobi462@gmail.com>
2022-03-20 00:09:29 +0900

    Update Root/Sources/Proposal/Internal/View/ProposalListView.swift

--- Root/Sources/Proposal/Internal/View/ProposalListView.swift
+++ Root/Sources/Proposal/Internal/View/ProposalListView.swift
@@ -34,7 +34,9 @@ struct ProposalListView: View {
                 }
                 .id(scrollToTopID)
             }
+            #if os(iOS)
             .listStyle(.insetGrouped)
+            #endif
         }
     }
 }
