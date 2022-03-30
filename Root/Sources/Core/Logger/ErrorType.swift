da1817c38983ed2fc7405756801109950bf757ce
Yusuke Hosonuma <tobi462@gmail.com>
2022-03-30 11:29:23 +0900

    refactor: separate file

--- Root/Sources/Core/Logger/ErrorType.swift
+++ Root/Sources/Core/Logger/ErrorType.swift
@@ -5,7 +5,6 @@
 //  Created by Yusuke Hosonuma on 2022/03/30.
 //
 
-import FirebaseCrashlytics
 import Foundation
 
 public enum ErrorType {
@@ -52,30 +51,5 @@ public enum ErrorType {
             code: code,
             userInfo: userInfo
         )
-    }
-}
-
-public final class Logger {
-    private init() {}
-
-    public static func error(_ error: ErrorType, filePath: StaticString = #file, line: UInt = #line) {
-        let fileName = String(filePath).split(separator: "/").last ?? "(unknown)" // 💡 It can't actually happen `nil`.
-        let path = "\(fileName)@L\(line)"
-        let message = "Logical Error: \(error) - \(path)"
-
-        // 💡 Assertion error to crash when development.
-        assertionFailure(message)
-
-        #if os(iOS)
-        Crashlytics.crashlytics().record(error: error.nsError(path: path))
-        #endif
-    }
-}
-
-private extension String {
-    init(_ staticString: StaticString) {
-        self = staticString.withUTF8Buffer {
-            String(decoding: $0, as: UTF8.self)
-        }
     }
 }
